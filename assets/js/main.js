349695daf230c66763441535c04fbd3763b2570a
Ubuntu <ubuntu@ip-172-31-87-142.ec2.internal>
2019-07-23 18:17:49 +0000

    Add image and caption functionality

---
+++ assets/js/main.js
@@ -0,0 +1,206 @@
+const gallery = [
+  {
+    "id": 001,
+    "image": "66784868_10157563789111180_787131483742535680_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 002,
+    "image": "67026241_10157563789246180_333276607332483072_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 003,
+    "image": "67048710_10157563789376180_1803840595932741632_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 004,
+    "image": "66772416_10157563789976180_8526035696362717184_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 005,
+    "image": "67098420_10157563790436180_6364629748663975936_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 006,
+    "image": "67479444_10157563790936180_514391061762670592_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 007,
+    "image": "67264048_10157563791556180_3474623856654680064_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 008,
+    "image": "66860129_10157563791981180_2973302582237200384_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 009,
+    "image": "66990516_10157563792586180_5513468200982937600_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 010,
+    "image": "66913640_10157563792786180_5384924902170034176_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 011,
+    "image": "67401813_10157563792981180_6253964474558971904_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 012,
+    "image": "67198502_10157563793116180_6759559043867475968_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 013,
+    "image": "67648093_488968975252797_2320456931791077376_n.jpg",
+    "student": "@Rishi S Rao"
+  }, {
+    "id": 014,
+    "image": "66466844_10157544475686180_5866344294570262528_n.jpg",
+    "student": "@Droid"
+  }, {
+    "id": 015,
+    "image": "66426657_10157544477531180_6319862256565747712_n.jpg",
+    "student": "@Droid"
+  }, {
+    "id": 016,
+    "image": "66654888_10219711806129727_4292988845084901376_n.jpg",
+    "student": "Akash Antony",
+    "description": "Style Transfer Image — at Sächsische Schweiz, Bastei."
+  }, {
+    "id": 017,
+    "image": "67148790_10157563782631180_578922296845008896_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 018,
+    "image": "67294054_10157563788941180_3130105975583801344_n.jpg",
+    "student": "@George Christopoulos"
+  }, {
+    "id": 019,
+    "image": "67324920_10157023383668127_8905717209125355520_n.jpg",
+    "student": "Elena Kutanov",
+    "description": "Grand Canyon in Malibu style"
+  }, {
+    "id": 020,
+    "image": "67350614_488968495252845_3267965655462182912_n.jpg",
+    "student": "@Rishi S Rao"
+  }, {
+    "id": 021,
+    "image": "67122960_3043216425719262_6790488499230343168_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 022,
+    "image": "67188625_3043218142385757_3232383380423180288_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 023,
+    "image": "67297169_3043219139052324_4597969177079185408_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 024,
+    "image": "67086504_3043220359052202_3529770141518659584_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 025,
+    "image": "67243502_3043221289052109_769185753170182144_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 026,
+    "image": "67186148_3043228202384751_3011656773334466560_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 027,
+    "image": "67427522_3043229522384619_3143776104867692544_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 028,
+    "image": "67241804_3043230129051225_1346571409500930048_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 029,
+    "image": "67171990_3043230905717814_1318584500657061888_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 030,
+    "image": "67237837_3043231819051056_2328263129340837888_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 031,
+    "image": "67279904_3043232439050994_5928328566801432576_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 032,
+    "image": "67118373_3043233149050923_4935075396296114176_n.jpg",
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
+  }, {
+    "id": 033,
+    "image": "67415842_10216620614642671_2538340060076441600_n.jpg",
+    "student": "Xenakis Karamanos‎"
+  }, {
+    "id": 034,
+    "image": "67064005_10156641323568391_5446607298329837568_n.jpg",
+    "student": "Anju Mercian-Alphonse"
+  }]
+
+console.log(gallery);
+
+
+var i;
+for (i = 0; i < gallery.length; i++) { 
+  // locate image for gallery item i
+  var path = "./assets/gallery_images/" + gallery[i].image
+  // locate section for gallery items
+  var section = document.getElementById('gallery');
+  
+  // create div with class flex
+  var div_flex = document.createElement('div');
+  div_flex.setAttribute('class', 'flex');
+  
+  // create div with class gallery
+  var div_gallery = document.createElement('div')
+  div_gallery.setAttribute('class', 'gallery');
+  
+  // create a with various attributes
+  var a = document.createElement('a');
+  a.setAttribute('target', "_blank");
+  a.setAttribute('href', "#");
+  
+  // create img
+  var img = document.createElement('img');
+  img.setAttribute('src', path);
+  img.setAttribute('alt', 'AI-generated image');
+  
+  // create div with class desc
+  var div_ballot = document.createElement('div');
+  div_ballot.setAttribute('class', 'desc');
+  var ballot = document.createTextNode(gallery[i].id);
+  
+  // create div with class desc
+  var div_student = document.createElement('div');
+  div_student.setAttribute('class', 'desc');
+  var student = document.createTextNode(gallery[i].student);
+  
+  // append flex div to section div
+  section.append(div_flex);
+  // append gallery div to flex div
+  div_flex.append(div_gallery);
+  // append a tag to gallery div
+  div_gallery.append(a);
+  // append img tag to a tag
+  a.append(img);
+  // append ballot text to ballot div
+  div_ballot.appendChild(ballot).textContent;
+  // append ballot div to gallery div
+  div_gallery.append(div_ballot);
+  
+  // append student text to student div
+  div_student.appendChild(student).textContent;
+  // append student div to gallery div
+  div_gallery.append(div_student);
+  
+  // for those gallery items with descriptions, create div with class desc
+  if(gallery[i].description) {
+    var div_desc = document.createElement('div')
+    div_desc.setAttribute('class', 'desc');
+    var desc = document.createTextNode(gallery[i].description);
+    div_desc.appendChild(desc).textContent;
+    div_gallery.appendChild(div_desc);
+  }
+}
