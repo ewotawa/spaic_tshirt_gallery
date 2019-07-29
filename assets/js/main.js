8b4addee0db573c2061b62ffc6cd3c629b4bf4df
Ubuntu <ubuntu@ip-172-31-87-142.ec2.internal>
2019-07-29 03:38:32 +0000

    added new images, updated ones verified to be in public domain

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,7 +136,7 @@ const gallery = [
   }, {
     "id": 34,
     "image": "67064005_10156641323568391_5446607298329837568_n.jpg",
-    "student": "Anju Mercian-Alphonse"
+    "student": "team unstoppable (@Agata [OR, USA], @Anju Mercian and @Sridhar Ethiraj)"
   }, {
     "id": 35,
     "image": "67431269_2613402908692812_5087633721276760064_n.jpg",
@@ -150,128 +150,343 @@ const gallery = [
     "image": "67564671_2613403008692802_5616531992859377664_n.jpg",
     "student": "Siddharth Srivastava"
   }, {
-  //   "id": 38,
-  //   "image": "67402897_2422085064551313_8427376737045708800_n.jpg",
-  //   "student": "team *unstoppable* (@Agata [OR, USA] , @Anju Mercian and Sridhar Ethiraj"
-  // }, {
     "id": 38,
+    "image": "67402897_2422085064551313_8427376737045708800_n.jpg",
+    "student": "team *unstoppable* (@Agata [OR, USA] , @Anju Mercian and Sridhar Ethiraj"
+  }, {
+    "id": 39,
     "image": "67274494_2613402872026149_2970268200727478272_n.jpg",
     "student": "Siddharth Srivastava"
   }, {
-    "id": 39,
+    "id": 40,
     "image": "67294043_2329985237260766_9009721972501774336_n.jpg",
     "student": "Juan Carlos Kuri Pinto",
     "description": "This image is a little bit clearer. I forgot to save the image directly by using <https://pytorch.org/docs/stable/torchvision/utils.html> instead of taking a screen capture.This is just part of my t-shirt design. I will post my complete design in few days. Tagging @Juan Carlos Kuri Pinto"
   }, {
-    "id": 40,
+    "id": 41,
     "image": "67105833_2329999593925997_8332475801098256384_n.jpg",
     "student": "Juan Carlos Kuri Pinto",
     "description": "Another brain whose abstract features are based on the mosaic painting. Tagging @Juan Carlos Kuri Pinto"
   }, {
-    "id": 41,
+    "id": 42,
     "image": "67259180_492451734904521_4657614869682454528_n.jpg",
     "student": "Elizabeth Wotawa",
     "description": "The photos are my own; styles come from two Edward Hopper paintings: Haskell House and Cape Cod Evening."
   }, {
-    "id": 42,
+    "id": 43,
     "image": "67823534_492451738237854_3660646599710736384_n.jpg",
     "student": "Elizabeth Wotawa",
     "description": "The photos are my own; styles come from two Edward Hopper paintings: Haskell House and Cape Cod Evening."
   }, {
-    "id": 43,
+    "id": 44,
     "image": "67125634_492451778237850_7942851159872503808_n.jpg",
     "student": "Elizabeth Wotawa",
     "description": "The photos are my own; styles come from two Edward Hopper paintings: Haskell House and Cape Cod Evening."
   }, {
-    "id": 44,
+    "id": 45,
     "image": "67101241_2330636007195689_7879127632044883968_n.jpg",
     "student": "Juan Carlos Kuri Pinto",
     "description": "Human brain + electric labyrinth style\nTagging @Juan Carlos Kuri Pinto"
   }, {
-    "id": 45,
+    "id": 46,
     "image": "67266696_10157586679801180_9196662258011209728_n.jpg",
     "student": "Droid"
   }, {
-    "id": 46,
+    "id": 47,
     "image": "67379091_10157586689516180_7259784143846768640_n.jpg",
     "student": "George Christospoulos"
   }, {
-    "id": 47,
+    "id": 48,
     "image": "67590227_10157586689681180_7544497921035599872_n.jpg",
     "student": "George Christospoulos"
   }, {
-    "id": 48,
+    "id": 49,
     "image": "67305769_10157586696851180_8207307397392236544_n.jpg",
     "student": "George Christospoulos"
   }, {
-    "id": 49,
+    "id": 50,
     "image": "67722786_10157586697051180_5357986476447498240_n.jpg",
     "student": "George Christospoulos"
   }, {
-    "id": 50,
+    "id": 51,
     "image": "67634976_10157586697306180_1763897984195493888_n.jpg",
     "student": "George Christospoulos"
   }, {
-    "id": 51,
+    "id": 52,
     "image": "67179173_10157586697496180_6656509068900827136_n.jpg",
     "student": "George Christospoulos"
   }, {
-    "id": 52,
+    "id": 53,
     "image": "67231650_10157586697731180_7271424381392257024_n.jpg",
     "student": "George Christospoulos"
   }, {
-    "id": 53,
+    "id": 54,
     "image": "67318760_2330636000529023_1516584344133566464_n.jpg",
     "student": "Juan Carlos Kuri Pinto",
     "description": "Human brain + colorful mosaic style\nTagging @Juan Carlos Kuri Pinto"
   }, {
-    "id": 54,
+    "id": 55,
     "image": "67249253_2330650017194288_7324000867063955456_n.jpg",
     "student": "Juan Carlos Kuri Pinto",
     "description": "Torch Brain\nTagging @Juan Carlos Kuri Pinto"
   }, {
-    "id": 55,
+    "id": 56,
     "image": "67294932_2330671207192169_3496032705257144320_n.jpg",
     "student": "Juan Carlos Kuri Pinto",
     "description": "Cryopreserved brain :snowflake: :brain: \n(This image is not real. I generated it by transferring ice style to a brain photo.)\nThe trick: Very low errors.\nIteration 300, Style Loss: 2.519400, Content Loss: 6.059410\nTagging @Juan Carlos Kuri Pinto"
   }, {
-    "id": 56,
+    "id": 57,
     "image": "67647416_10157586679476180_1690942166467084288_n.jpg",
     "student": "Droid"
   }, {
-    "id": 57,
+    "id": 58,
     "image": "67788803_10157586696436180_1581460825867026432_n.jpg",
     "student": "George Christospoulos"
   }, {
-    "id": 58,
+    "id": 59,
     "image": "67276022_493460498136978_7951270730291740672_n.jpg",
     "student": "Vigneshwari"
   }, {
-    "id": 59,
+    "id": 60,
     "image": "67404981_617262728763933_5566897783123738624_n.jpg",
     "student": "Seeratpal Kaur"
   }, {
-    "id": 60,
+    "id": 61,
     "image": "67473720_10157590591076180_6609307923778633728_n.jpg",
     "student": "George Christopoulos"
   }, {
-    "id": 61,
+    "id": 62,
     "image": "67225798_2380333545388562_7690633216914358272_n.jpg",
     "student": "Luis Antonio Aguilar Gutiérrez"
   }, {
-    "id": 62,
+    "id": 63,
     "image": "67168917_617308722092667_35934196069302272_n.jpg",
     "student": "Seeratpal Kaur"
   }, {
-    "id": 63,
+    "id": 64,
     "image": "67557886_494116991404662_2485447546416136192_n.jpg",
     "student": "Rishi S Rao",
     "description": "Wild Resonance"
   }, {
-    "id": 64,
+    "id": 65,
     "image": "67294926_494118141404547_2942628973781188608_n.jpg",
     "student": "Rishi S Rao",
     "description": "The Roar Beyond"
+  }, {
+    "id": 66,
+    "image": "67211580_2580878045472852_1844341488993959936_n.jpg",
+    "student": "Dharmendra Choudhary",
+    "description": "Taj Mahal"
+  }, {
+    "id": 67,
+    "image": "67632230_2429105423849277_8517968253132210176_n.jpg",
+    "student": "#60daysofudacity Project T-Shirt Contributed by team *unstoppable* (@Agata [OR, USA] , @Anju Mercian and Sridhar Ethiraj",
+    "description": "Cool cat in winter"
+  }, {
+    "id": 68,
+    "image": "67818259_2429105420515944_8782755752889548800_n.jpg",
+    "student": "#60daysofudacity Project T-Shirt Contributed by team *unstoppable* (@Agata [OR, USA] , @Anju Mercian and Sridhar Ethiraj",
+    "description": "Angry cat in Summer"
+  }, {
+    "id": 69,
+    "image": "67471984_10156881902266725_6669792947480821760_n.jpg",
+    "student": "Fustincho",
+    "description": "Submission by Fustincho: Space skull. Original: https://www.instagram.com/p/Bop7Lthg0VV/  DISCLAIMER: ORIGINAL IMAGES BELONG TO MY BEST FRIEND. NO NOT USE WITHOUT PERMISSSION."
+  }, {
+    "id": 70,
+    "image": "67294501_10156881902456725_3724044203945099264_n.jpg",
+    "student": "Fustincho",
+    "description": "Submission by Fustincho: Spring woman II. Original: https://www.instagram.com/p/Biij_ZggLYZ/ DISCLAIMER: ORIGINAL IMAGES BELONG TO MY BEST FRIEND. NO NOT USE WITHOUT PERMISSSION."
+  }, {
+    "id": 71,
+    "image": "67458990_10156881902646725_3285444501614624768_n.jpg",
+    "student": "Fustincho",
+    "description": "Submission by Fustincho: Black bird. Original: https://www.instagram.com/p/BWYhilJFA6E/ DISCLAIMER: ORIGINAL IMAGES BELONG TO MY BEST FRIEND. NO NOT USE WITHOUT PERMISSSION."
+  }, {
+    "id": 72,
+    "image": "67543830_10156881902656725_684111921439309824_n.jpg",
+    "student": "Fustincho",
+    "description": "Submission by Fustincho: Spring traveler. Original: https://www.instagram.com/p/BW_ncpTlBrZ/ DISCLAIMER: ORIGINAL IMAGES BELONG TO MY BEST FRIEND. NO NOT USE WITHOUT PERMISSSION."
+  }, {
+    "id": 73,
+    "image": "67503916_10156881902931725_4797414131021905920_n.jpg",
+    "student": "Fustincho",
+    "description": "Submission by Fustincho: Spring woman.  Original: https://www.instagram.com/p/Bop7Lthg0VV/ DISCLAIMER: ORIGINAL IMAGES BELONG TO MY BEST FRIEND. NO NOT USE WITHOUT PERMISSSION."
+  }, {
+    "id": 74,
+    "image": "67216096_10156881903151725_3990800551070662656_n.jpg",
+    "student": "Fustincho",
+    "description": "Submission by Fustincho: Fire witch.  Original: https://www.instagram.com/p/Beow9bBFNph/ DISCLAIMER: ORIGINAL IMAGES BELONG TO MY BEST FRIEND. NO NOT USE WITHOUT PERMISSSION."
+  }, {
+    "id": 75,
+    "image": "67393795_10156881903211725_3006769091961683968_n.jpg",
+    "student": "Fustincho",
+    "description": "Submission by Fustincho: Fire ravens  Original: https://www.instagram.com/p/BW30F0vl1WB/ DISCLAIMER: ORIGINAL IMAGES BELONG TO MY BEST FRIEND. NO NOT USE WITHOUT PERMISSSION."
+  }, {
+    "id": 76,
+    "image": "67386928_10156881932481725_7010841865071099904_n.jpg",
+    "student": "Fustincho",
+    "description": "Submission by Fustincho: Shadow Jaguar  Original: https://www.instagram.com/p/BWWiJB_l5QN/ DISCLAIMER: ORIGINAL IMAGES BELONG TO MY BEST FRIEND. NO NOT USE WITHOUT PERMISSSION."
+  }, {
+    "id": 77,
+    "image": "67225215_10157592821071180_7989033366953394176_n.jpg",
+    "student": "Sarah Majors"
+  }, {
+    "id": 78,
+    "image": "67789331_10220088788996846_8902134231442915328_n.jpg",
+    "student": "Berenice Terwey",
+    "description": "Atlantis – by Berenice. Used a oil on canvas painting by my dad as style image"
+  }, {
+    "id": 81,
+    "image": "67526165_494676301348731_3338178646945824768_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "And here comes the second part: The same my drawings to my paintings .. a couple of them are anatomy studies although a little bit scary  Hope you like them!"
+  }, {
+    "id": 82,
+    "image": "67381183_494676324682062_1318452615096303616_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "Here are my 10 first images for the project. I decided to go with something different. I'm using some of my old drawings and paintings as content and style images. I didn't expect the result to be so fancy"
+  }, {
+    "id": 83,
+    "image": "67230799_494676428015385_4769615668163117056_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "Here are my 10 first images for the project. I decided to go with something different. I'm using some of my old drawings and paintings as content and style images. I didn't expect the result to be so fancy"
+  }, {
+    "id": 84,
+    "image": "67402432_494676448015383_5214917495160307712_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "And here comes the second part: The same my drawings to my paintings .. a couple of them are anatomy studies although a little bit scary  Hope you like them!"
+  }, {
+    "id": 85,
+    "image": "68246552_494676458015382_4319052978674728960_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "And here comes the second part: The same my drawings to my paintings .. a couple of them are anatomy studies although a little bit scary Hope you like them!"
+  }, {
+    "id": 86,
+    "image": "67715127_494676564682038_7213285977496748032_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "And here comes the second part: The same my drawings to my paintings .. a couple of them are anatomy studies although a little bit scary  Hope you like them!"
+  }, {
+    "id": 87,
+    "image": "67743385_494676608015367_3648084593464049664_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "And here comes the second part: The same my drawings to my paintings .. a couple of them are anatomy studies although a little bit scary  Hope you like them!"
+  }, {
+    "id": 88,
+    "image": "67413932_494676621348699_8560035963147911168_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "And here comes the second part: The same my drawings to my paintings .. a couple of them are anatomy studies although a little bit scary  Hope you like them!"
+  }, {
+    "id": 89,
+    "image": "67698535_494676671348694_4277609881457917952_n.jpg",
+    "student": "Samuela Anastasi ",
+    "description": "Here are my 10 first images for the project. I decided to go with something different. I'm using some of my old drawings and paintings as content and style images. I didn't expect the result to be so fancy"
+  }, {
+    "id": 90,
+    "image": "67251283_494676721348689_8162329507731079168_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "Here are my 10 first images for the project. I decided to go with something different. I'm using some of my old drawings and paintings as content and style images. I didn't expect the result to be so fancy"
+  }, {
+    "id": 91,
+    "image": "67343285_494676731348688_8339670923586043904_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "And here comes the second part: The same my drawings to my paintings .. a couple of them are anatomy studies although a little bit scary  Hope you like them!"
+  }, {
+    "id": 92,
+    "image": "67647123_494676754682019_4518151513440256000_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "Here are my 10 first images for the project. I decided to go with something different. I'm using some of my old drawings and paintings as content and style images. I didn't expect the result to be so fancy"
+  }, {
+    "id": 93,
+    "image": "67257740_494676811348680_5436906242813460480_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "Here are my 10 first images for the project. I decided to go with something different. I'm using some of my old drawings and paintings as content and style images. I didn't expect the result to be so fancy"
+  }, {
+    "id": 94,
+    "image": "67357966_494676838015344_8010012869150113792_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "Here are my 10 first images for the project. I decided to go with something different. I'm using some of my old drawings and paintings as content and style images. I didn't expect the result to be so fancy"
+  }, {
+    "id": 95,
+    "image": "67664277_494676854682009_2722047566651850752_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "Here are my 10 first images for the project. I decided to go with something different. I'm using some of my old drawings and paintings as content and style images. I didn't expect the result to be so fancy"
+  }, {
+    "id": 96,
+    "image": "67265729_494676888015339_1848841326229979136_n.jpg",
+    "student": "Samuela Anastasi ",
+    "description": "Here are my 10 first images for the project. I decided to go with something different. I'm using some of my old drawings and paintings as content and style images. I didn't expect the result to be so fancy"
+  }, {
+    "id": 97,
+    "image": "67240644_494676918015336_1053873374008180736_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "Here are my 10 first images for the project. I decided to go with something different. I'm using some of my old drawings and paintings as content and style images. I didn't expect the result to be so fancy"
+  }, {
+    "id": 98,
+    "image": "67298919_2580538268840163_3105610909511319552_n.jpg",
+    "student": "Dharmendra Choudhary"
+  }, {
+    "id": 99,
+    "image": "67225001_2354375384647364_4531013029231329280_n.jpg",
+    "student": "Sourav Kumar",
+    "description": "Project : #sg_project-t-shirt #60daysofudacity #udacityfacebookscholar Sourav Kumar 'Earth covered in Lava'"
+  }, {
+    "id": 100,
+    "image": "67588432_2580878015472855_1451243396046782464_n.jpg",
+    "student": "Dharmendra Choudhary",
+    "description": "Taj Mahal"
+  }, {
+    "id": 101,
+    "image": "67365190_2354820191269550_7172785599831605248_n.jpg",
+    "student": "Sourav Kumar",
+    "description": "Project : #sg_project-t-shirt #60daysofudacity #udacityfacebookscholar Sourav Kumar 'New planet discovered , yet to be verified by NASA'"
+  }, {
+    "id": 102,
+    "image": "67921329_2354822147936021_6219593088239665152_n.jpg",
+    "student": "Sourav Kumar",
+    "description": "Project : #sg_project-t-shirt #60daysofudacity #udacityfacebookscholar Sourav Kumar 'Reliving Sir Isaac Newton' — with Isaac Newton."
+  }, {
+    "id": 103,
+    "image": "67813345_2429105410515945_9192551325768876032_n.jpg",
+    "student": "#60daysofudacity Project T-Shirt Contributed by team *unstoppable* (@Agata [OR, USA] , @Anju Mercian and Sridhar Ethiraj",
+    "description": "Cat in spring season"
+  }, {
+    "id": 104,
+    "image": "67229818_10156881902076725_561157302791438336_n.jpg",
+    "student": "Fustincho",
+    "description": "Submission by Fustincho: Dark ravens Original: https://www.instagram.com/p/BW30F0vl1WB/ DISCLAIMER: ORIGINAL IMAGES BELONG TO MY BEST FRIEND. NO NOT USE WITHOUT PERMISSSION."
+  }, {
+    "id": 105,
+    "image": "68302422_10156881932396725_2749836069481480192_n.jpg",
+    "student": "Fustincho",
+    "description": "Submission by Fustincho: Fire Jaguar. Original: https://www.instagram.com/p/BWWiJB_l5QN/ DISCLAIMER: ORIGINAL IMAGES BELONG TO MY BEST FRIEND. NO NOT USE WITHOUT PERMISSSION."
+  }, {
+    "id": 106,
+    "image": "67913058_10157592820606180_2720412283623702528_n.jpg",
+    "student": "Sarah Majors"
+  }, {
+    "id": 107,
+    "image": "67260193_617802808709925_86000698558251008_n.jpg",
+    "student": "Seeratpal Kaur"
+  }, {
+    "id": 108,
+    "image": "67374946_10220088788916844_9134196167758839808_n.jpg",
+    "student": "Berenice Terwey",
+    "description": "Anturias & Roses – by @Berenice"
+  }, {
+    "id": 109,
+    "image": "67513980_494667911349570_7892056394138910720_n.jpg",
+    "student": "Lisa Crossman"
+  }, {
+    "id": 110,
+    "image": "67286143_494669548016073_3547102937308200960_n.jpg",
+    "student": "Lisa Crossman",
+    "description": "Renoir in the style of M Escher"
+  }, {
+    "id": 111,
+    "image": "67649704_494676254682069_8056105401608830976_n.jpg",
+    "student": "Samuela Anastasi",
+    "description": "And here comes the second part: The same my drawings to my paintings .. a couple of them are anatomy studies although a little bit scary  Hope you like them!"
   }
 ]
 
